c5f710c3517216d571af5c75255926fbd85f36dc
marvsticks <marvsticks123@gmail.com>
2024-07-01 10:20:51 +0100

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -107,9 +107,46 @@ rate = float(inputed_rate/100)
 time = float(input("time"))
 
 amount = principal * (1 + rate)**time
-
-
 print(amount)
+
+
+
+assigmnet for the weekend
+
+#a super store makes sales of five goods
+
+#phones, freezer, television, Air conditioner, batteries
+
+cost_price_of_phone = 72000
+cost_price_of_freezer = 150000
+cost_price_of_television = 120000
+cost_price_of_Air_conditioner = 500000
+cost_price_of_battery = 200000
+
+selling_price_of_phone = int(input('Cost price of phone = 72000 What is your selling price'))
+selling_price_of_frezzer=int(input('Cost Cost price of freezer = 150000 What is your selling price '))
+selling_price_of_television = int(input('Cost price of television = 120000 What is your selling price'))
+selling_price_of_Air_conditioner = int(input('Cost price of Air conditioner = 500000 What is your selling price'))
+selling_price_of_battery = int(input('Cost price of battery = 200000 What is your selling price'))
+
+
+total_selling_price = selling_price_of_phone + selling_price_of_frezzer + selling_price_of_television + selling_price_of_Air_conditioner + selling_price_of_battery
+
+total_cost_price = cost_price_of_phone + cost_price_of_freezer + cost_price_of_television + cost_price_of_Air_conditioner + cost_price_of_battery
+
+
+
+
+
+if total_selling_price > total_cost_price:
+	print('You have made profit of ', total_selling_price - total_cost_price)
+elif total_selling_price < total_cost_price:
+	print('You have made a loss of ', total_cost_price - total_selling_price)
+
+
+
+assignment 2
+
 
 #Folade is a student that offers 10 courses, at the end of the semster are scores are below
 #input scores  wrtie a program to calculate her standing in the class 70 above A,60-69 B, 50-59 C, 40-49 D, 30-39 E, 29-0 F
@@ -126,32 +163,28 @@ score_for_crk = int(input('Please input the score for crk'))
 score_for_history = int(input('Please input the score for history'))
 score_for_bookkeeping = int(input('Please input the score for bookkeeping'))
 
-subjects_in_list = [score_for_agric, score_for_biology, score_for_bookkeeping, score_for_chemistry, score_for_civic, score_for_crk, score_for_economics, score_for_english, score_for_history, score_for_maths, score_for_physics')
-
-print(list(filter(lamda x: x>100, subjects_in_list)))input the score for bookkeeping'))
-
-subjects_in_list = [score_for_agric, score_for_biology, score_for_bookkeeping, score_for_chemistry, score_for_civic, score_for_crk, score_for_economics, score_for_english, score_for_history, score_for_maths, score_for_physics')
-
-print(list(filter(lamda x: x>100, subjects_in_list)))input the score for bookkeeping'))
-
-subjects_in_list = [score_for_agric, score_for_biology, score_for_bookkeeping, score_for_chemistry, score_for_civic, score_for_crk, score_for_economics, score_for_english, score_for_history, score_for_maths, score_for_physics')
-
-print(list(filter(lamda x: x>100, subjects_in_list)))
-
-each_scores = (score_for_agric + score_for_biology + score_for_bookkeeping + score_for_chemistry + score_for_civic + score_for_crk + score_for_economics + score_for_english + score_for_history + score_for_maths + score_for_physics)/100
-
-print(each_scores)
+each_scores = (score_for_agric + score_for_biology + score_for_bookkeeping + score_for_chemistry + score_for_civic + score_for_crk + score_for_economics + score_for_english + score_for_history + score_for_maths + score_for_physics)/10
 
 if each_scores >= 70:
-	print(' You are among the best of the best you have A')
-
-elif each_scores > 60 <69:
-	print ('You have done well, you have B')
+    print('You are among the best, You have A')
+elif each_scores >60 < 69:
+    print('You have done well, you have B')
 elif each_scores >50 <69:
-	print('Do better next time you have C')
-elif each_scores >40 <49:
-	print('Your performance was poor, you have D')
+    print('Do better next time, You have C')
+elif each_scores > 40 <49:
+    print('Your performance was bad, you have D')
 elif each_scores >30 <39:
-	print('very bad form you, you have E')
+    print('Very bad from you, you have E')
 elif each_scores >0 <20:
-	print('worst performance, you have F and you are repeating this class')
+    print('Worst performance, you have F you are repeating this class')
+
+
+
+
+
+
+
+
+
+
+
